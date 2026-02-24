7a2c2db2dfef0f012a5e42f5411417cfcdb6eac3
PedroMegiolaro <pe.mbraga@gmail.com>
2026-02-23 22:15:55 -0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+#print("calcule seu peso ideal")
+#altura = float(input("digite sua altura: "))
+#pesoIdeal = (72.7 * altura) - 58
+#print("seu peso ideal é: ", pesoIdeal)
+
+#print("saiba o preço do carro")
+#KmPercorrido = float(input("digite a quantidade de quilometros percorridos: "))
+#preçoDia = int(input("digite a quuantidade de dias: "))
+#preçoTotal = (0.15 * KmPercorrido) + (60 * preçoDia)
+#print("Total a pagar: ", preçoTotal)
+
+#dias = int(input("digite a quuantidade de dias: "))
+#horas = int(input("digite a quuantidade de horas: "))
+#minutos = int(input("digite a quuantidade de minutos: "))
+#segundos = int(input("digite a quuantidade de segundos: "))
+#segundosTotal = ( 86400 * dias) + (3600 * horas) + (60 + minutos) + segundos
+#print("Tempo total foi de ", segundosTotal, "segundos")
+
+#ganhaHr = float(input("digite o valor da hora de trabalho: "))
+#hrsTrabalhadas = float(input("digite o numero de horas trabalhadas: "))
+#salarioBruto = hrsTrabalhadas * ganhaHr
+#ir = salarioBruto * 0.11
+#inss = salarioBruto * 0.08
+#sindicato = salarioBruto * 0.05
+#salarioLiquido = salarioBruto - (ir + inss + sindicato)
+#print("Salario Bruto: R$ ", salarioBruto)
+#print("IR (11%) : R$ ", ir)
+#print("INSS (8%) : R$ ", inss)
+#print("Sindicato (5%) : R$ ", sindicato)
+#print("Salario liquido : R$ ", salarioLiquido)
